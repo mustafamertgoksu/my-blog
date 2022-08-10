d00f1f8e30d2793e0a33b6e6e3342efcccd3bb92
Mustafa Göksu <98381051+mustafamertgoksu@users.noreply.github.com>
2022-08-10 15:01:28 +0300

    fix the errors

---
+++ comments.tsx
@@ -0,0 +1,24 @@
+import Giscus from '@giscus/react';
+import styles from '../styles/comments.module.css';
+import { useToggleTheme } from '../hooks/useToggleTheme';
+
+const Comments = () => {
+  const { theme } = useToggleTheme();
+  const isLightMode = theme === 'light';
+  return (
+    <div className={styles.comments}>
+      <Giscus
+        repo="mustafamertgoksu/my-blog"
+        repoId="R_kgDOHzAtew"
+        category="General"
+        categoryId="DIC_kwDOHzAte84CQuhn"
+        mapping="pathname"
+        reactionsEnabled="0"
+        emitMetadata="0"
+        theme="dark"
+      />
+    </div>
+  );
+};
+
+export default Comments;
