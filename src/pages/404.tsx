5a6fe37e157a8d1dcfb4f15333b84bc85a34d461
Mustafa Mert Göksu <98381051+mertgoksu@users.noreply.github.com>
2022-08-23 18:04:01 +0300

    change 404 page text

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -11,8 +11,8 @@ const Error = () => {
   return (
     <div className="p-4 flex justify-center items-center flex-col">
       <h4 className="text-3xl mb-4">
-        <span className="text-red-500">404 not found</span> -{' '}
-        <span className="text-blue-500"> are your lose you way?</span>
+        <span className="text-red-500">Oops!</span> -{' '}
+        <span className="text-blue-500">We can't find that page</span>
       </h4>
       <button
         className="bg-green-900 text-white roundex py-4 px-4"
