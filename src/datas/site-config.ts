1e3693986cc730ac55e52d28557ad0905ba4b830
Mustafa Göksu <98381051+mustafamertgoksu@users.noreply.github.com>
2023-09-04 11:04:32 +0300

    Update site-config.ts

--- src/datas/site-config.ts
+++ src/datas/site-config.ts
@@ -11,5 +11,5 @@ export const Datas: DatasType = {
     "Mustafa Mert Göksu is a frontend developer based in Ankara, Turkey",
   info: "Student, Developer",
   aboutParagraph:
-    "I'm a student and frontend developer in Turkey. I love making software. I like playing chess and solving cube. I'm currently looking for a high school. ",
+    "I'm a student and frontend developer in Turkey. I love making software. I like playing chess. I'm currently trying to improve my development skills. ",
 };
